f79394c2672697e9e047deda62202ed0dcac1964
Yem Daro <100412991+darororo@users.noreply.github.com>
2024-07-14 19:09:46 +0700

    Basic authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "batmon", password: "pepe", except: [:index, :show]
   def index
     @articles = Article.all
   end
